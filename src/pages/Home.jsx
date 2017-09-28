2a812b7ec1bf482e57a8e9f85143fc07cda4cefb
allen4dev <alanaliagadev@gmail.com>
2017-09-27 23:20:13 -0400

    [Add] flow type check

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
+// @flow
+
+import * as React from 'react';
 
 import src from './allen_walker.jpg';
 
-function Home() {
-  return (
-    <section className="Home">
-      <h1 className="Home-title">You are in Home</h1>
-      <div className="Home-photo">
-        <img className="Home-image" src={src} alt="allen4dev avatar" />
-      </div>
-    </section>
-  );
+type State = {
+  dummie: string,
+};
+
+class Home extends React.Component<{}, State> {
+  state = {
+    dummie: '',
+  };
+
+  render() {
+    return (
+      <section className="Home">
+        <h1 className="Home-title">You are in Home</h1>
+        <div className="Home-photo">
+          <img className="Home-image" src={src} alt="allen4dev avatar" />
+        </div>
+      </section>
+    );
+  }
 }
 
 export default Home;
